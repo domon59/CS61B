8db3068acb6d26209b168b688edac53886c28daa
domon59 <lyf115@gmail.com>
2017-03-30 15:05:15 -0500

    fix

--- lab1/HelloNumbers.java
+++ lab1/HelloNumbers.java
@@ -1,7 +1,7 @@
 package com.yuanfeng.induction;
 
 /**
- * Created by yuanfengliu on 3/28/17.
+ * Created by yuanfeng on 3/28/17.
  *
  *  Modify HelloNumbers so that it prints out the cumulative sum of the integers from 0 to 9.
  *  For example, your output should start with 0 1 3 6 10... and should end with 45.
